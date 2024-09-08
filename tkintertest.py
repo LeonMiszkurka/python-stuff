77dc583361c8d9f1dcee175b52113db69eee90e8
Alex <alexmis8@gmail.com>
2024-09-07 18:09:44 -0700

    Adding countdown bar

--- tkintertest.py
+++ tkintertest.py
@@ -2,18 +2,41 @@ import tkinter as tk
 import time as t
 from math import *
 
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
 def evaluate(event):
     input = entry.get()
     secret_value = "execute virus"
     if (input == secret_value):
-        countdown = 9
+        countdown = 0
         print("loading...virus.runtime")
         print("loading")
         print("laoding_runtime-x/bool")
 
         t.sleep(1)
-        for x in range(countdown, 0, -1):
-            print(str(x) + "...")
+        for x in range(countdown, 11):
+            #print(str(x) + "...")
+            printProgressBar(x, 10)
             t.sleep(1)
         while (bool(1)):
             print("virus.runtime is runing")
